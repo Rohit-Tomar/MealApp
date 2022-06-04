2752cc6f027a8e1d4c20fd4d17e0ebe447b3cc1b
Rohit Tomar <rohittomar@Rohits-MacBook-Air.local>
2022-06-04 15:38:25 +0530

    build static UI

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered to you</h2>
+      <p>
+        Lorem ipsum dolor sit amet. Non quam dolor ut cupiditate quia ut harum
+        adipisci est veritatis repellat non earum aperiam est quae rerum.
+      </p>
+      <p>
+        et velit repellat non earum aperiam est quae rerum. Hic beatae omnis 33
+        earum sint ex ducimus officiis?
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
